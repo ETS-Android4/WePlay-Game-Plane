abd99fb18252ff4f8da8c7936c68c1c0444e4be3
sens <senswrong@gmail.com>
2022-02-09 17:06:35 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,107 @@
+# WePlay Game SDK
+
+see com.weplay.game.GameActivity from [DEMO](https://github.com/WePlayGaming/WePlay-Game-Plane)
+
+https://github.com/WePlayGaming/WePlay-Game-Plane
+
+## 1.Step One init
+
+### 1. Add aar library
+
+![image](aar.png)
+
+### 2. implementation aar
+
+```groovy
+dependencies {
+    implementation fileTree(dir: 'libs', include: ['*.aar'])
+    implementation 'com.google.code.gson:gson:2.8.0'
+}
+```
+
+### 3. proguard
+
+```groovy
+##############################  WePlay  ################################
+-keep class com.weplay.WePlay{public static <methods>;}
+-keep public interface com.weplay.WePlay$*{*;}
+-keep class com.weplay.WePlayServer{public <methods>;}
+-keep class com.weplay.message.**{*;}
+## Gson
+-keep class com.google.gson.stream.** { *; }
+-keepattributes EnclosingMethod
+
+-keepclassmembers enum * {
+  public static **[] values();
+  public static ** valueOf(java.lang.String);
+}
+-keep class * extends java.lang.Enum { *; }
+-keep interface * extends java.lang.Enum { *; }
+```
+
+### 4. set YOUR_APP_KEY
+
+```xml
+<application
+    android:name=".GameApplication">
+    <meta-data
+        android:name="WE_PLAY_APP_KEY"
+        android:value="${YOUR_APP_KEY}" />
+</application>
+```
+
+### 5. initialization WePlay SDK
+
+```java
+public class GameApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WePlay.init(this);
+    }
+}
+```
+## 2.Step Two use
+
+### 1. Auto Login with WePlay
+
+```java
+WePlay.setOnAccountCallback(new WePlay.OnAccountCallback() {
+    @Override
+    public boolean onLogin(WePlayUser wePlayUser) {
+        System.out.println(wePlayUser.userId + "<>" + wePlayUser.userName);
+        return true;//if use this login info return true
+    }
+
+    @Override
+    public boolean onLogout() {
+        return true;//if logout return true
+    }
+});
+```
+
+### 2. send gamer's score
+
+```java
+WePlay.sendScore((long)score)
+```
+
+### 3. Great Moment
+
+```java
+WePlay.greatMoment()
+```
+
+### 4. Advertisement
+
+```java
+//show rewardedVideo ad
+WePlay.showAd(AdType.TYPE_REWARDED_VIDEO, adId);
+//show interstitial ad
+WePlay.showAd(AdType.TYPE_INTERSTITIAL, null);
+//listen ad callback
+WePlay.setOnAdCallback(adId -> {
+    //Reward users
+});
+```
+
--- game-plane/src/main/java/com/weplay/game/GameActivity.java
+++ game-plane/src/main/java/com/weplay/game/GameActivity.java
@@ -18,21 +18,39 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         gameView = new GameView(this);
         setContentView(gameView);
-        //WePlay.sendScore(long score)
+        /*
+        WePlay.sendScore(long score)
+        上报分数
+        */
         gameView.setOnScoreCallback(WePlay::sendScore);
+        /*
+        WePlay.greatMoment()
+        精彩时刻(当前时刻回退15s的录屏内容)
+        */
         gameView.setGreatMomentCallback(WePlay::greatMoment);
         gameView.setAdCallback(new GameView.OnShowAdCallback() {
             @Override
             public void showInterstitialAd() {
+                /*
+                 * 展示插屏广告
+                 * */
                 WePlay.showAd(AdType.TYPE_INTERSTITIAL, null);
             }
 
             @Override
             public void showRewardedVideoAd() {
                 adId = String.valueOf(System.currentTimeMillis());
+                /*
+                 * 展示激励广告
+                 * 自定义adId，用户观看后会回调adId
+                 * */
                 WePlay.showAd(AdType.TYPE_REWARDED_VIDEO, adId);
             }
         });
+        /*
+         * 用户观看激励广告的回调
+         * adID 广告Id
+         * */
         WePlay.setOnAdCallback(adId -> {
             if (adId.equals(GameActivity.this.adId)) {
                 GameActivity.this.adId = null;
@@ -40,8 +58,15 @@ public class GameActivity extends Activity {
                 gameView.addBomb();
             }
         });
-        gameView.start();
+        /*
+         * 登陆与退出回调
+         * */
         WePlay.setOnAccountCallback(new WePlay.OnAccountCallback() {
+            /*
+             * 登陆
+             * WePlay主动下达用户资料，游戏收到资料，执行登陆
+             * 如果接受该用户登陆 返回 true
+             * */
             @Override
             public boolean onLogin(WePlayUser wePlayUser) {
                 gameView.setUserName(wePlayUser.userName + "@WePlay");
@@ -49,6 +74,9 @@ public class GameActivity extends Activity {
                 return true;
             }
 
+            /*
+             * 可以退出返回 true
+             * */
             @Override
             public boolean onLogout() {
                 gameView.setUserName(null);
@@ -57,6 +85,7 @@ public class GameActivity extends Activity {
                 return true;
             }
         });
+        gameView.start();
     }
 
     @Override
